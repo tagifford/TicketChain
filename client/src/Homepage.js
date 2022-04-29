24458d4b161a70cc1f827ade2ac69fd3c016cb37
= <=>
2022-04-29 10:31:56 -0600

    initial setup

---
+++ client/src/Homepage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './css/Homepage.css';
+
+function Homepage() {
+
+  return (
+    <div className="Homepage">
+      <header className="Homepage-header">
+        Welcome to TicketChain.
+         <button className="btn">Get a Wallet</button>
+      </header>
+
+    </div>
+  );
+}
+
+
+export default Homepage;
