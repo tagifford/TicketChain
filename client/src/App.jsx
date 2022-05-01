4fba3b6174f036d0520876fc8b23d3555e1a58a0
Kiera Jost <kieraj@rams.colostate.edu>
2022-05-01 17:27:47 -0600

    styling and button errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,105 +1,114 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import './css/Style.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./css/Style.css";
+import logo from "./logo.png";
 
 function Wallet() {
   return (
-    <div className="header">
+    <div className="default">
       <button className="btn">Get a wallet</button>
     </div>
-  )
+  );
 }
 
 function Event() {
   return (
-    <div className="header">
-      <header>
-        Create an event
-      </header>
-      <form>
+    <div className="default">
+      <div className="header">Create an event</div>
+      <form className="content" onSubmit={handleSubmit}>
         <div className="content">
           <label>
             Event name:
-            <input type="text" name="event_name"/>
+            <input type="text" name="event_name" />
           </label>
           <label>
             Number of tickets:
-            <input type="text" name="num_tickets"/>
+            <input type="text" name="num_tickets" />
           </label>
           <label>
             Ticket price:
-            <input type="text" name="ticket_price"/>
+            <input type="text" name="ticket_price" />
           </label>
-          <input type="submit" name="submit" className="btn"/>
-          </div>
+          <br></br>
+          <input type="submit" name="submit" className="btn" />
+        </div>
       </form>
     </div>
-  )
+  );
 }
 
+async function handleSubmit(event) {}
+
 function Tickets() {
-  return(
-    <div className="header">
-      <Ticket name="Example Event"/>
+  return (
+    <div className="default">
+      <Ticket name="Example Event" />
     </div>
-  )
+  );
 }
 
 function Ticket(props) {
-  return(
+  return (
     <div className="content">
-      <a>{props.name}</a>
-      <input type="submit" name="transfer" value="Transfer" className="btn"/>
+      <a className="header">{props.name}</a>
+      <input type="submit" name="transfer" value="Transfer" className="btn" />
     </div>
-  )
+  );
 }
 
 function Purchase() {
   return (
-    <div className="header">
-      <header>Purchase a ticket</header>
-      <form>
-        <div className="content">
-          <label>
-            Seller address:
-            <input type="text" name="seller_addr"/>
-          </label>
-          <label>
-            Buyer address:
-            <input type="text" name="buyer_addr"/>
-          </label>
-          <label>
-            Quantity:
-            <input type="text" name="quantity"/>
-          </label>
-          <input type="submit" name="purchase_submit" className="btn"/>
-        </div>
+    <div className="default">
+      <div className="header">Purchase a ticket </div>
+      <form className="content">
+        <label>
+          Seller address:
+          <input type="text" name="seller_addr" />
+        </label>
+        <label>
+          Buyer address:
+          <input type="text" name="buyer_addr" />
+        </label>
+        <label>
+          Quantity:
+          <input type="text" name="quantity" />
+        </label>
+        <br></br>
+        <input type="submit" name="purchase_submit" className="btn" />
       </form>
     </div>
-  )
+  );
 }
 
 function Welcome() {
   return (
     <div className="default">
-      <header className="header">
-        Welcome to TicketChain.
-      </header>
+      <header className="header">Welcome to TicketChain.</header>
+      <img className="logo" src={logo} alt="Logo" />
     </div>
   );
 }
 
-
 function Homepage() {
-  return(
+  return (
     <div className="navbar">
       <Router>
         <div>
-          <Link to="/" exact className="link">Home</Link>
-          <Link to="/wallet" className="link">Get a wallet</Link>
-          <Link to="/purchase" className="link">Purchase tickets</Link>
-          <Link to="/event" className="link">Create an event</Link>
-          <Link to="/tickets" className="link">View your tickets</Link>
+          <Link to="/" exact className="link">
+            Home
+          </Link>
+          <Link to="/wallet" className="link">
+            Get a wallet
+          </Link>
+          <Link to="/purchase" className="link">
+            Purchase tickets
+          </Link>
+          <Link to="/event" className="link">
+            Create an event
+          </Link>
+          <Link to="/tickets" className="link">
+            View your tickets
+          </Link>
         </div>
         <Routes>
           <Route path="/" element={<Welcome />} />
@@ -109,9 +118,8 @@ function Homepage() {
           <Route path="/purchase" element={<Purchase />} />
         </Routes>
       </Router>
-      </div>
-  )
+    </div>
+  );
 }
-
 
 export default Homepage;
